7b6a4a4a58fb5955a45130591b3344e77afe5bae
Dipesh <Dipesh.K@sparinfosys.com>
2023-09-27 16:31:17 +0530

    Add erb template, route, and create url logic

--- app/controllers/url_histories_controller.rb
+++ app/controllers/url_histories_controller.rb
@@ -1,4 +1,22 @@
 class UrlHistoriesController < ApplicationController
   def index
+    @url_histories = UrlHistory.all.order(created_at: :desc)
+  end
+
+  def create
+    @url_history = UrlHistory.new(url_history_params)
+    
+    if @url_history.save
+      redirect_to url_histories_path, notice: 'URL was successfully saved.'
+    else
+      flash[:alert] = 'URL could not be saved.'
+      redirect_to url_histories_path
+    end
+  end
+
+  private
+
+  def url_history_params
+    params.require(:url_history).permit(:url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root 'url_histories#index'
+
+  resources :url_histories, only: [:index, :create] do
+    collection do
+      get 'email_report', to: 'url_histories#email_report'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
